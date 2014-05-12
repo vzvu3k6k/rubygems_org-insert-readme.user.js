bb38a7a29c495283c1672a284241835a22da2132
vzvu3k6k <vzvu3k6k@gmail.com>
2014-05-12 23:44:42 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var fileInclude = require('gulp-file-include');
+var packageInfo = require('./package.json');
+var slugify = require('slugify2');
+var replace = require('gulp-replace');
+
+var paths = {
+  scripts: 'scripts/**/*.js',
+  styles: 'styles/**/*.scss'
+};
+
+gulp.task('scripts', ['styles'], function() {
+  return gulp.src(paths.scripts)
+    .pipe(replace(/^(\/\/ @\w+\s+){{(\w+)}}$/mg, function(substr, pre, key){
+      return pre + (packageInfo.userscript[key] || packageInfo[key] || "");
+    }))
+    .pipe(fileInclude({basepath: '@file'}))
+    .pipe(concat(slugify(packageInfo.name) + '.user.js'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src(paths.styles)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('style.css', {newLine: ''}))
+    .pipe(gulp.dest('tmp/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.styles, ['styles', 'scripts']);
+});
+
+gulp.task('default', ['scripts', 'watch']);
---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name           {{name}}
+// @description    {{description}}
+// @version        {{version}}
+// @author         {{author}}
+// @match          http://rubygems.org/gems/*
+// @match          https://rubygems.org/gems/*
+// @namespace      {{namespace}}
+// @license        {{license}}
+// ==/UserScript==
+
+var a = document.querySelectorAll('.meta .links a');
+var repo;
+for(var i = 0; i < a.length; i++)
+  if((repo = a[i].href.match(/https?:\/\/github\.com\/([^/]+)\/([^/]+)/)))
+    break;
+
+if(repo){
+  if(typeof GM_info == 'undefined'){
+    function GM_info(){
+      var script = {};
+      ['name', 'version'].forEach(function(key){
+        if(GM_getMetadata(key))
+          script[key] = GM_getMetadata(key)[0];
+      });
+      return {script: script};
+    }
+  }
+  var userAgent = GM_info().script.name + ' (' + GM_info().script.version + ')';
+
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://api.github.com/repos/' + repo[1] + '/' + repo[2] + '/readme',
+    headers: {'User-Agent': userAgent, 'Accept': 'application/vnd.github.v3.html+json'},
+    onload: function(result){
+      GM_addStyle("@@include('../tmp/style.css')");
+      document.querySelector('.border:last-of-type')
+        .insertAdjacentHTML('afterend', '<div class="border readme_box">' + result.responseText + '</div>');
+    }
+  });
+}
